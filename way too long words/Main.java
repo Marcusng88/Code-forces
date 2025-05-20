258a5196c1110f51d74a5b5160de570f3ca1f89f
Marcus Ng <ngzhengjie888@gmail.com>
2025-05-20 21:21:33 +0800

    added way too long words solution

---
+++ way too long words/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < n; i++) {
+            String x = input.nextLine();
+            if (x.length() < 10) {
+                System.out.println(x);
+            } else {
+                System.out.println(x.charAt(0) + String.valueOf(x.length() - 2) + x.charAt(x.length() - 1));
+            }
+        }
+    }
+}
